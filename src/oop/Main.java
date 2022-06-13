b165edf4280eb90bdf8d849e5ad0c7ed9e16d4de
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-13 21:36:31 +0200

    running

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -1,5 +1,9 @@
 package oop;
 
 public class Main {
+    public static void main(String[] args) {
+        Salary salary = new Salary("Kareem", "Kgl",120, 100000);
 
+        salary.computePay();
+    }
 }
