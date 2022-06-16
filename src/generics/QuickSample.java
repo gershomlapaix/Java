1928667f6dbd23eeb6c5aa2f24d18a9e37923f58
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-16 13:11:56 +0200

    sample contents

--- src/generics/QuickSample.java
+++ src/generics/QuickSample.java
@@ -1,4 +1,21 @@
 package generics;
 
-public class QuickSample {
+public class QuickSample <T,U>{
+    T obj1;
+    U obj2;
+
+    QuickSample(T obj1, U obj2){
+        this.obj1 = obj1;
+        this.obj2 = obj2;
+    }
+
+    public void print(){
+        System.out.println(obj1);
+        System.out.println(obj2);
+    }
+
+    public static void main(String[] args) {
+        QuickSample<String, Integer> sample= new QuickSample("Rwanda", 1283);
+        sample.print();
+    }
 }
