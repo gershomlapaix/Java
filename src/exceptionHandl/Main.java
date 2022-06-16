b16f3bb2ca2f24abd6183037a09032004a307548
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-16 11:18:34 +0200

    exceptions

---
+++ src/exceptionHandl/Main.java
@@ -0,0 +1,23 @@
+package exceptionHandl;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            int a[] = new int[7];
+            a[9] = 30/0;
+            System.out.println("Statement in try block");
+
+        }
+
+        catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("we don't have such array");
+        }
+
+        catch (ArithmeticException e){
+            System.out.println("You can not devide number by 0");
+        }
+        catch (Exception e){
+            System.out.println("SOme other exception");
+        }
+    }
+}
--- src/oop/composition/Honda.java
+++ src/oop/composition/Honda.java
@@ -1,12 +1,17 @@
 package oop.composition;
 
 public class Honda extends Car {
+    private int price;
+
+    public Honda(){};
+    public Honda(int price){
+        this.price = price;
+    };
     public void HondaStart(){
         CarEngine hondaEngine = new CarEngine();  // composition here
         hondaEngine.stopEngine();
     }
-
-    public static void main(String[] args) {
+    public static void main(String ...Honda) {
         Honda honda = new Honda();
 
         honda.setColour("Black");
