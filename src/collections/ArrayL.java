55e0113cb1f0bc6ea6ae98ca8a4c5d72007567d6
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-06-16 14:27:56 +0200

    iteration

--- src/collections/ArrayL.java
+++ src/collections/ArrayL.java
@@ -1,4 +1,27 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ArrayL {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList();
+
+        System.out.println("Initial size of array = "+ al.size());
+
+        al.add(101);
+        al.add(102);
+        al.add(103);
+        al.add(1, 140);
+
+//        retrieval
+
+        Iterator <Integer> iterate = al.iterator();
+        int number = iterate.next();
+        System.out.println("Accessed element: "+number);
+
+        while (iterate.hasNext()){
+            iterate.forEachRemaining((value)-> System.out.println(value+" "));
+        }
+    }
 }
